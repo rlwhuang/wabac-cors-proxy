1080950521aab3a7cb1b8fd10318d50b30834a77
rlwhuang <lh1677@nyu.edu>
2025-01-06 00:06:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const CORS_ALLOWED_ORIGINS = [
   "http://localhost:8000",
   "https://oldweb-cjk-cx3ee.ondigitalocean.app",
   "https://oldweb-cjk.switcheristic.tel",
-  "https://oldweb-cjk.swtch.tel"
+  "https://oldweb-cjk.pastinter.net"
 ];
 
 
